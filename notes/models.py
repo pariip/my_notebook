48e1c37146e1e4427435894a06b7612b3b62e5a3
parip <fateme.it89@gmail.com>
2020-06-15 20:59:43 +0430

    add table note for save notes information

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from users.models import CustomUser
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    public_note = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(default=timezone.now())
+    updatedAt = models.DateTimeField(default=timezone.now())
+    deletedAt = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
