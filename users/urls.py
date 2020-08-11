2db0872231631ab64d3e90a3ad4f7c4af44238e6
parip <fateme.it89@gmail.com>
2020-08-11 12:57:41 +0430

    update url with add name for signIn,signUp links

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signIn', views.SignIn.as_view()),
-    path('signUp', views.SignUp.as_view()),
+    path('signIn', views.SignIn.as_view(), name='signIn'),
+    path('signUp', views.SignUp.as_view(), name='signUp'),
 
 ]
